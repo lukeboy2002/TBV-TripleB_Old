2fbab207cfd00fcdf2e00e22a604e5e3b922e809
Lukeboy_2002 <35039280+lukeboy2002@users.noreply.github.com>
2023-09-09 22:36:27 +0200

    Model and seeders

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -62,4 +63,9 @@ class User extends Authenticatable implements MustVerifyEmail
     protected $appends = [
         'profile_photo_url',
     ];
+
+    public function member(): HasOne
+    {
+        return $this->hasOne(Member::class);
+    }
 }
---
+++ database/seeders/members/BudSeeder.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Database\Seeders\members;
+
+use App\Models\Member;
+use App\Models\Role;
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class BudSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $user = User::create([
+            'username' => 'Bud',
+            'email' => 'bud@tbv-tripleB.nl',
+            'email_verified_at' => now(),
+            'password' => Hash::make('budbudbud'),
+            'remember_token' => Str::random(10),
+        ]);
+
+        Member::create([
+            'user_id' => $user->id,
+            'image' => "members/Bud.jpeg"
+        ]);
+
+        //generate image
+        $username = get_initials($user->username);
+        $id = $user->id.'.png';
+        $path = '/profile-photos/';
+        $imagePath = create_avatar($username, $id, $path);
+
+        //save image
+        $user->profile_photo_path = $imagePath;
+        $user->save();
+
+        $role = Role::select('id')->where('name', 'member')->first();
+
+        $user->roles()->attach($role);
+    }
+}
